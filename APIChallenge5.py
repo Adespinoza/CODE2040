eb24e242acbeac4290569ae404e9fa202721d647
Adam Espinoza <adespinoza97@gmail.com>
2016-10-24 12:27:13 -0700

    Added additional comments to APIChallenge5

--- APIChallenge5.py
+++ APIChallenge5.py
@@ -4,19 +4,21 @@ import json
 import datetime 
 from datetime import timedelta 
 
+#Set variables from web URLs 
 web = 'http://challenge.code2040.org/api/dating'
 validationWeb = 'http://challenge.code2040.org/api/dating/validate'
 
 dateObject = requests.post(web, json={'token':'af9b409be63eedf2b016a42e76ad2253'})
 dateObject = dateObject.json()
 
+#Initialize variables from dateObject 
 dateStamp = dateObject['datestamp']
 intervalToAdd = dateObject['interval']
 
 #Convert interval to add in to seconds 
 intervalToAdd = timedelta(seconds=intervalToAdd)
 
-#Converst string into the form, looked up form 
+#Converts string into the form, looked up form 
 timePeriod = datetime.datetime.strptime(dateStamp, '%Y-%m-%dT%H:%M:%SZ')
 
 #Add two times together 
@@ -25,4 +27,5 @@ totalTime = timePeriod + intervalToAdd
 #Convert back time period given
 totalTime = totalTime.strftime('%Y-%m-%dT%H:%M:%SZ')
 
+#Send requests post
 r = requests.post(validationWeb, json= {'token':'af9b409be63eedf2b016a42e76ad2253', 'datestamp': totalTime})
