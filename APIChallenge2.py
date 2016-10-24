b26775ca23f8b975954b12509ddd2a6e65bde1a2
Adam Espinoza <adespinoza97@gmail.com>
2016-10-24 12:26:37 -0700

    Added additional comments to APIChallenge2

--- APIChallenge2.py
+++ APIChallenge2.py
@@ -4,10 +4,12 @@ import json
 web = 'http://challenge.code2040.org/api/reverse'
 validationWeb = 'http://challenge.code2040.org/api/reverse/validate'
 
+#Initialize string received 
 string = requests.post(web, data = {'token':'af9b409be63eedf2b016a42e76ad2253'})
 
 # Reverse the string 
 stringReversed = string.text[::-1]
 
+#Send request to server
 req = requests.post(validationWeb, data={'token': 'af9b409be63eedf2b016a42e76ad2253' , 'string' : stringReversed})
 
