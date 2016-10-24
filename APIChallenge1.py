615616a85c412e0220abb163c8788ba1ab3d379f
Adam Espinoza <adespinoza97@gmail.com>
2016-10-24 12:26:26 -0700

    Added additional comments to APIChallenge1

--- APIChallenge1.py
+++ APIChallenge1.py
@@ -1,10 +1,13 @@
 import requests
 import json
 
+#Initialize varliables 
 web = 'http://challenge.code2040.org/api/register'
 token = 'af9b409be63eedf2b016a42e76ad2253'
 registration = 'https://github.com/Adespinoza/CODE2040'
 
+#Create a form object to pass to JSON
 form = {'github': registration, 'token': token}
 
+#Send request posts
 req = requests.post(web, json=form)
